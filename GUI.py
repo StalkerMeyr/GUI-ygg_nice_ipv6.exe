c199cddfdd3e3850b40e8dbff0244c1b80d5d1a9
averyanalex <averyanalex@gmail.com>
2020-07-29 11:35:52 +0300

    Улучшен запуск и исправлен баг

--- GUI.py
+++ GUI.py
@@ -1,15 +1,30 @@
 from tkinter import *
+from tkinter import filedialog, messagebox
 import subprocess
 
 root = Tk()
 root.geometry('340x230')
 root.resizable(width=False, height=False)
-root.title("GUI генератора IPv6 от Vikulin")
+root.title("GUI для генератора IPv6 от Vikulin")
+
+already_running = False
 
 
 def start():
-    subprocess.Popen("ygg_nice_ipv6.exe" + " " + matching_bytes_field.get() + " " + ipv6_limit_field.get() + " " + what_to_search_field.get())
-    root.destroy()
+    global already_running
+    if not already_running:
+        already_running = True
+        ygg_nice_ipv6_exe_path = filedialog.askopenfilename(filetypes=(("Ygg_nice_ipv6.exe", "*.exe"),))
+        subprocess.Popen(
+            f"{ygg_nice_ipv6_exe_path} {matching_bytes_field.get()} {ipv6_limit_field.get()} {what_to_search_field.get()}",
+            stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, stdin=subprocess.DEVNULL)
+        # пока вывод в никуда, потом сделаем консоль
+        messagebox.showinfo("Запущено", "Не закрывайте GUI, так как это остановит генератор. Как узнать ключи ("
+                                        "результат работы "
+                                        "программы), мы ещё не придумали.")
+        # из-за root.destroy() гуи закрывается, и все субпроцессы тоже
+    else:
+        messagebox.showerror("Ошибка", "Генератор уже работает!")
 
 
 repeated_bytes = Label(root, text="Число повторяющихся байтов")
