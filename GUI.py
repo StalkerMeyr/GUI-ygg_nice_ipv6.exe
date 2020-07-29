e652065b8bacfa9a7e7435f649c8f10c2ca5a6ea
averyanalex <averyanalex@gmail.com>
2020-07-29 11:08:53 +0300

    Улучшена читабельность

--- GUI.py
+++ GUI.py
@@ -1,37 +1,39 @@
 from tkinter import *
 import subprocess
+
 root = Tk()
 root.geometry('340x230')
 root.resizable(width=False, height=False)
-root.title("Генератор vikulin GUI")
-a=" " #Костыли наше все:)
+root.title("GUI генератора IPv6 от Vikulin")
+
+
 def start():
-    subprocess.Popen("ygg_nice_ipv6.exe"+a+text1.get()+a+text2.get()+a+text3.get())
+    subprocess.Popen("ygg_nice_ipv6.exe" + " " + matching_bytes_field.get() + " " + ipv6_limit_field.get() + " " + what_to_search_field.get())
     root.destroy()
 
-powtor_bite=Label(root,text="Число повторяющихся байтов")
-text1=Entry(root,width=11,font='Arial 14')
 
-gen=Label(root,text="Сколько ipv6 будет пройдено")
-text2=Entry(root,width=11,font='Arial 14')
+repeated_bytes = Label(root, text="Число повторяющихся байтов")
+matching_bytes_field = Entry(root, width=11, font='Arial 14')
 
-key=Label(root,text="Совпадающие части")
-text3=Entry(root,width=11,font='Arial 14',)
-text_pred=Label(root,text="(Можно оставить пустым)")
+gen = Label(root, text="Сколько ipv6 будет пройдено")
+ipv6_limit_field = Entry(root, width=11, font='Arial 14')
 
-go=Button(text="Запуск программы",width=15,height=3,bg="#99746C",fg='#FFFFFF',font='arial 14',command=start)
+key = Label(root, text="Совпадающие части")
+what_to_search_field = Entry(root, width=11, font='Arial 14', )
+text_pred = Label(root, text="(Можно оставить пустым)")
 
-text1.place(x=180,y=1)
-powtor_bite.place(x=0,y=1)
+go = Button(text="Запуск программы", width=15, height=3, bg="#99746C", fg='#FFFFFF', font='arial 14', command=start)
 
-gen.place(x=0,y=26)
-text2.place(x=180,y=26)
+matching_bytes_field.place(x=180, y=1)
+repeated_bytes.place(x=0, y=1)
 
-key.place(x=0,y=52)
-text3.place(x=180,y=52)
-text_pred.place(x=0,y=70)
+gen.place(x=0, y=26)
+ipv6_limit_field.place(x=180, y=26)
 
-go.place(x=80,y=120)
+key.place(x=0, y=52)
+what_to_search_field.place(x=180, y=52)
+text_pred.place(x=0, y=70)
+
+go.place(x=80, y=120)
 
 root.mainloop()
-
